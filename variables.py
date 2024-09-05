9dd317f0223f5114487dd8768757de568247262c
hb7464 <hb7464@rit.edu>
2024-09-05 21:13:10 +0400

    hw

---
+++ variables.py
@@ -0,0 +1,8 @@
+def prompt_print():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    print(a,"+",b,"=",a+b)
+    print(a,"-",b,"=",a-b)
+    print(a,"x",b,"=",a*b)
+    print(a,"/",b,"=",a/b)
+prompt_print()
