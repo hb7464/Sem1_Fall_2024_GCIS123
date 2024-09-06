3f4625087452ad6d728646b39eea61ac3ad3c1c6
hb7464 <hb7464@rit.edu>
2024-09-06 09:21:55 +0400

    prac

--- variables.py
+++ variables.py
@@ -5,4 +5,15 @@ def prompt_print():
     print(a,"-",b,"=",a-b)
     print(a,"x",b,"=",a*b)
     print(a,"/",b,"=",a/b)
-prompt_print()
+
+def variables_prac():
+    ageinmonths = int(input("How old are you? \n")) *12
+    numofdaysinyear = 365 
+    nameoffirstpet = input("Enter the name of your first pet: ")
+    first5digitsofpi = 3.1415
+    print(ageinmonths)
+    print(numofdaysinyear)
+    print(nameoffirstpet)
+    print(first5digitsofpi)
+
+variables_prac()
