c76e44901f22fb34e33001d87b690213e85430e9
hb7464 <hb7464@g.rit.edu>
2024-09-25 10:28:48 +0400

    sdsd

--- misc/test2.py
+++ misc/test2.py
@@ -1,3 +1,24 @@
-#quick test 
-print('test2')
-print('update 1')
+def ind(s):
+    print(s)
+    i=0
+    while i < len(s):
+        print(s[i])
+        i+=1
+
+def rev(s):
+    print(s)
+    re=''
+    i=1
+    while i <= len(s):
+        print(s[-i])
+        re += s[-i]
+        i+=1
+    print(re)
+    
+def main():
+    s=input("Enter a string: ")
+    ind(s)
+    print()
+    rev(s)
+
+main()
