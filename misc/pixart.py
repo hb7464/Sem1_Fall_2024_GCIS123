f7c8d4172e656af806cdf81dc05ddcfe6afd7f28
hb7464 <hb7464@g.rit.edu>
2024-10-02 09:43:27 +0400

    s

--- misc/pixart.py
+++ misc/pixart.py
@@ -25,13 +25,23 @@ def box():
     t.up()
     t.forward(Pixel_Size)
 
-def row():
+def row(X):
     c=0
-    while c < COLUMNS:
+    while c < X:
         box()
         c+=1
-        
+
+def colour(x,y,length,col):
+    t.up()
+    t.goto(-300+(x*Pixel_Size),300-(y*Pixel_Size))
+    t.begin_fill()
+    t.fillcolor(col)
+    t.down()
+    t.goto(-300+(x*Pixel_Size),300-(y*Pixel_Size))
+    row(length)
+
 def main():
+    t.tracer(False)
     t.Screen().setup(width=0.5, height=0.75, startx=None, starty=None)
     intialization()
     i = 0
@@ -39,7 +49,13 @@ def main():
         i+=1
         t.up()
         t.goto(-300,300-(i*Pixel_Size))
-        row()
+        row(COLUMNS)
+    t.tracer(True)
+    a = int(input("Enter x coordinate: "))
+    b = int(input("Enter y coordinate: "))
+    c = int(input("Enter no of boxes: "))
+    d = input("Enter colour: ")
+    colour(a,b,c,d)
     input()
 
 main()
