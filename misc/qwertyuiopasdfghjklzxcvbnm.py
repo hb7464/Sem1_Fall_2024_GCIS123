0cf82acf82a07fa911dce6d5954b7c306c62a8ce
hb7464 <hb7464@g.rit.edu>
2024-10-02 10:02:07 +0400

    qwertyuiopasdfhjklzxcvbnm

--- misc/qwertyuiopasdfghjklzxcvbnm.py
+++ misc/qwertyuiopasdfghjklzxcvbnm.py
@@ -1,2 +1,35 @@
-for i in range(10,-1,-1):
-    print(i)
+def rev1(s):
+    a = len(s) - 1
+    s2=''
+    for i in range(a,-1,-1):
+        s2=s2+s[i]
+    print(s2)
+
+def rev2(s):
+    a = len(s)+1
+    s2=''
+    for i in range(1,a):
+        s2=s2+s[-i]
+    print(s2)
+
+def rev3(s):
+    a = len(s)
+    s2=''
+    for i in range(0,a):
+        s2=s[i]+s2
+    print(s2)
+
+def rev4(s):
+    a = len(s)+1
+    for i in range(1,a):
+        print(s[-i],end='')
+
+def main():
+    s=input("Enter string: ")
+    rev1(s)
+    rev2(s)
+    rev3(s)
+    rev4(s)
+
+if __name__ == '__main__':
+    main()
