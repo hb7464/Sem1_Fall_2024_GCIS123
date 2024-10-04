dd1e7ed46383cf59067bb1373bf9844f6c30f2f1
hb7464 <hb7464@g.rit.edu>
2024-10-04 10:52:03 +0400

    s

--- misc/filetime.py
+++ misc/filetime.py
@@ -1,3 +1,20 @@
-with open(r"C:\Users\hisha\gcis123\hb7464\misc\tl.txt") as f:
-    for line in f:
-        print(line.split())
+def search():
+    s = input("Enter the word to be searched: ")
+    with open(r"C:\Users\hisha\gcis123\hb7464\misc\tl.txt") as f:
+        c=0
+        ch=0
+        for line in f:
+            c+=1
+            a = line.split()
+            for i in a:
+                if s==i:
+                    print(f"Word found in line {c}")
+                    ch+=1
+        if ch == 0:
+            print("Word not found")
+
+def main():
+    search()
+
+if __name__ == '__main__':
+    main()
