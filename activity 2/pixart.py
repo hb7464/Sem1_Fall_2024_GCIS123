a4ee2185749be321b37c6c0bf872a37a1a92eb09
hb7464 <hb7464@g.rit.edu>
2024-10-13 18:40:36 +0400

    s

--- activity 2/pixart.py
+++ activity 2/pixart.py
@@ -1,58 +1,65 @@
-import turtle as t
+'''
+Baahir's Repository link - https://github.com/Baahir2007/GCIS.git
+Hishaam's Repository link - https://github.com/hb7464/hb7464 
+Munzir's Repository link - https://github.com/munzir1910/GCIS123 
+'''
 
+'''A module containing all the functions required
+to draw either from user input or a file'''
 
-def get_colour(n):
+'''Intializing the global variables'''
+PIXEL_SIZE = 30                     #Length of each pixel side
+ycoor = PIXEL_SIZE*10               #The y-coordinate from which pixels start drawing from
+xcoor = -PIXEL_SIZE*10              #The x-coordinate from which pixels start drawing from
 
-    '''A function designed to take 
-    in a character and return a colour'''
+def get_color(color_string):
+        
+        """Function takes a number/letter and applies a color to that specific number/letter"""  
+        
+        if color_string == "0":
+            return 'black'
+        elif color_string == "1":
+            return 'white'
+        elif color_string == "2":
+            return 'red'
+        elif color_string == "3":
+            return 'yellow'
+        elif color_string == "4":
+            return 'orange'
+        elif color_string == "5":
+            return 'green'
+        elif color_string == "6":
+            return 'yellowgreen'
+        elif color_string == "7":
+            return 'sienna'
+        elif color_string == "8":
+            return 'tan'
+        elif color_string == "9":
+            return 'gray'
+        elif color_string == "A":
+            return 'darkgrey'
+        else:
+            return None
 
-    if n == '0':
-        return 'black'
-    elif n == '1':
-        return 'white'
-    elif n == '2':
-        return 'red'
-    elif n == '3':
-        return 'yellow'
-    elif n == '4':
-        return 'orange'
-    elif n == '5':
-        return 'green'
-    elif n == '6':
-        return 'yellowgreen'
-    elif n == '7':
-        return 'sienna'
-    elif n == '8':
-        return 'tan'
-    elif n == '9':
-        return 'gray'
-    elif n == 'A':
-        return 'darkgray'
-
-def draw_color_pixel(color_string, turta):
-
-    '''A function designed to draw a 
-    pixel with the inputed color'''
+def draw_color_pixel(color_string,turta):
     
-    turta.setheading(0)
-    turta.pencolor('black')
+    """This function makes one pixel and fills it with a color that has been provided"""
+    
+    turta.down()
     turta.fillcolor(color_string)
     turta.begin_fill()
-        
-    for j in range(4):
-        turta.forward(20)
-        turta.left(90)
-
-    turta.forward(20)
+    for i in range(4):
+        turta.forward(PIXEL_SIZE)
+        turta.right(90)
+    turta.forward(PIXEL_SIZE)
     turta.end_fill()
 
-def draw_pixel(color_string_number, turta):
+def draw_pixel(color_string_number,turta):
     
-    '''A function that uses the draw_color_pixel function and get colour 
-    function in tandem to draw a pixel of a user desired color'''
-
-    color_string = get_colour(color_string_number)
-    draw_color_pixel(color_string,turta)
+    """This function makes one pixel and fills it with a color that has been provided"""
+    
+    color_string = get_color(color_string_number)
+    draw_color_pixel(color_string, turta)
 
 def draw_line_from_string(color_string_line, turta):  
 
@@ -70,39 +77,50 @@ def draw_line_from_string(color_string_line, turta):
 
 def draw_shape_from_string(turta):
 
-    '''A function designed that takes user input continuously
-     and draws pixel of the associated colors to the string 
-     until the user inputs an empty string or an invalid 
-     character not associated with any color'''
+    """ A function that prompts a color string 
+        from the user under a loop until the user 
+        enters an empty string or if that color is 
+        not defined and it is the initial step for 
+        drawing the colored pixel """
     
+    y = 0
     while True:
-        
-        color_string = input("Enter a color string: ")
-        
-        if color_string == '' or draw_line_from_string(color_string, turta) == False:
+        Row_Color = input("Enter a color string: ").strip()
+        turta.penup()
+        turta.goto(-200, 200 - y)
+        turta.pendown()
+        turta.speed(0)
+        Rows = draw_line_from_string (Row_Color, turta)
+        if Row_Color == "" or Rows == False:
             break
+        else:
+            y += PIXEL_SIZE
         
 def draw_grid(turta):
-
-    '''A function that draws a grid with a 
-    black and red checkerboard pattern using 
-    the draw_line_from_string function'''
-
-    turta.speed(0)
-    turta.up()
-    turta.goto(-200,100)
-
-    for i in range(20):
-        
-        turta.down()
-        if i%2 == 0:
-            draw_line_from_string('02020202020202020202',turta)
-        else:    
-            draw_line_from_string('20202020202020202020',turta)
-            
-        turta.up()
-        t.goto(-200,100-((i+1)*20))
     
+    """A function that draws a 
+       20x20 color grid"""
+
+    Color1 = input("Enter color1: ")
+    Color2 = input("Enter color2: ")
+    String1 = (Color1 + Color2)*10
+    String2 = (Color2 + Color1)*10
+    row = 0
+    while row < 10:
+        y = 0
+        for i in range(10):
+            turta.penup()
+            turta.goto(xcoor,ycoor - y)
+            turta.pendown()
+            draw_line_from_string (String1, turta)
+            y += PIXEL_SIZE
+            turta.penup()
+            turta.goto(xcoor,ycoor - y)
+            turta.pendown()
+            draw_line_from_string (String2, turta)
+            y += PIXEL_SIZE
+            row += 1
+                        
 def draw_shape_from_file(turta):
     
     '''A function designed to take a file path 
@@ -118,16 +136,8 @@ def draw_shape_from_file(turta):
         Row=0
         
         for line in file:
-        
-            turta.goto(-200,200-(20*Row))
+
+            turta.up()
+            turta.goto(xcoor,ycoor-(PIXEL_SIZE*Row))
             draw_line_from_string(line, turta)
             Row += 1
-    
-def main():
-    t.tracer(False)
-    t.speed(0)
-    draw_shape_from_file(t)
-    input()
-
-if __name__ == '__main__':
-    main()
