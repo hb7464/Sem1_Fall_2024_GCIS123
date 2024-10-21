c80b2af0b0546ff3bbc3ddfa1e3b27518a73c13c
hb7464 <hb7464@g.rit.edu>
2024-10-21 11:25:28 +0400

    s

---
+++ misc/array1.py
@@ -0,0 +1,32 @@
+import arrays
+
+def making_arrays():
+    array1 = arrays.Array(5)
+    array2 = arrays.Array(1,0)
+    array3 = arrays.Array(10, "")
+    array4 = arrays.Array(20,False)
+    print(array1,array2,array3,array4,sep ='\n')
+
+def for_fill():
+    arr = arrays.Array(10)
+    leng = len(arr)
+    for i in arr:
+        arr[i] = i+1
+    print(arr)
+
+def while_fill():
+    arr = arrays.Array(10)
+    leng = len(arr)
+    c = 0
+    while c < leng:
+        arr[c] = c+1
+        c+=1
+    print(arr)
+
+def main():
+    # making_arrays()
+    # for_fill()
+    while_fill()
+
+if __name__ == '__main__':
+    main()
