f9cd8951dc740f1522e6986aefbd9b2ac604c3ea
hb7464 <hb7464@g.rit.edu>
2024-10-25 09:50:29 +0400

    still hate recursion

--- misc/binarysearchrecursion.py
+++ misc/binarysearchrecursion.py
@@ -2,39 +2,50 @@ def binary_search(L,top,bot,val):
 
     L.sort()
     leng = len(L)
-    mid = (top-bot)//2
+    mid = (top+bot)//2
     
-    # print(f"{L} \n Top: {top} \n Bottom: {bot} \n Mid: {mid} \n MidValue: {L[mid]} \n Value: {val}")
+    #print(f"{L} \n Top: {top} \n Bottom: {bot} \n Mid: {mid} \n MidValue: {L[mid]} \n Value: {val}")
    
     if L[mid] == val:
-        
+     
         return mid
-
+    
+    elif bot > top:
+     
+        return -1
+    
     elif L[mid] < val: 
-
-        bot = mid
+     
+        bot = mid+1
         return binary_search(L,top,bot,val)
 
     elif L[mid] > val:
-
-        top = mid
+     
+        top = mid - 1
         return binary_search(L,top,bot,val)
     
+    elif bot > top:
+     
+        return -1
     
 def main():
-    try:
 
-        # L = eval(input("LIST: "))
-        # val = int(input("Value to be found: "))
-        L = [0,1,2,3,4,5,6,7,8,9]
-        val = 10
-        top = len(L) - 1
-        a=binary_search(L,top,0,val)
-        print(f"{val} was found at index {a}")
-
-    except RecursionError:
-
+    # L = eval(input("LIST: "))
+    # val = int(input("Value to be found: "))
+    
+    L = [0,1,2,3,4,5,6,7,8,9]
+    val = 10
+    top = len(L) - 1
+    a=binary_search(L,top,0,val)
+    
+    if a == -1:
+    
         print(f"{val} is not a value in the list")
+    
+    else:
+    
+        print(f"{val} was found at index {a}")
+    
 
 if __name__ == '__main__':
     main()
