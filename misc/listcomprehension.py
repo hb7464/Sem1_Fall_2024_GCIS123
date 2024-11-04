518199e319ccca83ebb18897cecb2b31baafbf93
hb7464 <hb7464@g.rit.edu>
2024-11-04 10:10:41 +0400

    comprehend. NOW!

---
+++ misc/listcomprehension.py
@@ -0,0 +1,9 @@
+even_number = [x for x in range(10)if x%2 == 0] 
+print(even_number)
+
+#prints [0,2,4,6,8]
+
+odd_number = [x for x in range(10)if x%2 != 0] 
+print(odd_number)
+
+#prints [1,3,5,7,9]
