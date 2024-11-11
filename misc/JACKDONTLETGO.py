b2e7c354b37f30863ff24e7a5546abea34683e97
hb7464 <hb7464@g.rit.edu>
2024-11-11 10:35:19 +0400

    NOOOOOOO JACK

--- misc/JACKDONTLETGO.py
+++ misc/JACKDONTLETGO.py
@@ -12,7 +12,7 @@ def check_first(adict,key):
     print(end-st)
 def main():
     check_first(adict,'EIIIIIII')
-    check_first(adict,'de')
+    check_first(adict,'Celine Dion')
 
 if __name__ == '__main__':
     main()
