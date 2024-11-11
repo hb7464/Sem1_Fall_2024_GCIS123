1cd90c435e3ff5e9ad0f254e9214937dc79b54ab
hb7464 <hb7464@g.rit.edu>
2024-11-11 10:24:40 +0400

    fixed it

--- misc/hashingstart.py
+++ misc/hashingstart.py
@@ -5,7 +5,8 @@ def set_hashing(aset):
         print(f'{item} has hash value: {hash(item)}')
 
 def main():
-    seta = {1,34,3,21,333}
+    seta = {1,34,3,21,333} #Doesn't work since the values are integers instead of literally anything else
+                            #Put something like strings inside it and it will return an actual hash
     set_hashing(seta)
 
 if __name__ == '__main__':
