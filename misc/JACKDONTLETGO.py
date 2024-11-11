fe8f22289c7f0418826e1260137f69c488b74bd9
hb7464 <hb7464@g.rit.edu>
2024-11-11 10:31:59 +0400

    NO JACK

--- misc/JACKDONTLETGO.py
+++ misc/JACKDONTLETGO.py
@@ -3,9 +3,13 @@ adict = {'and I':2, 'EIIIIIII':3,'WILLL ALWAYYYYYYYYS':1,'LOOOOOOOOVE':5,'YOOOOO
 value = adict['EIIIIIII']
 
 def check_first(adict,key):
+    import time
+    st= time.perf_counter_ns()
     if key in adict:
         value = adict[key]
         print(value)
+    end = time.perf_counter_ns()
+    print(end-st)
 def main():
     check_first(adict,'EIIIIIII')
     check_first(adict,'de')
