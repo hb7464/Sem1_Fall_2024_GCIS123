86020df6abf5cfb97aea2977b062f19360a3a2e0
hb7464 <hb7464@g.rit.edu>
2024-11-15 10:35:37 +0400

    wtf

---
+++ misc/classesintro.py
@@ -0,0 +1,18 @@
+class student:
+
+    def printvalues(student):
+        print(f"Name: {student.name} \nAge: {student.age} \nHeight: {student.height} \nGPA: {student.gpa} ")
+
+student1 = student()
+student1.name = 'william'
+student1.age = 17
+student1.height = 178
+student1.gpa = 3.17
+student1.printvalues()
+
+student2 = student()
+student2.name = 'steve'
+student2.height = 166
+student2.age = 32
+student2.gpa = 3.23
+student2.printvalues()
