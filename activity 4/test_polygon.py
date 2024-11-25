41a0dfcdcbacf3218f1f42af3e619a27312a8749
hb7464 <hb7464@g.rit.edu>
2024-11-25 09:17:57 +0400

    commit

--- activity 4/test_polygon.py
+++ activity 4/test_polygon.py
@@ -1,43 +1,6 @@
 import pytest
-import polygon 
-
-class Polygon_Test:
-    
-    __slots__ = ['__name', '__sides']
-
-    def __init__(self, name, sides):
-        self.__name = name
-        self.__sides = sides
-
-    def get_name(self):
-        return self.__name
-
-    def set_name(self,name):
-        self.__name = name
-
-    def get_sides(self):
-        return self.__sides
-        
-    def set_sides(self,sides):
-        self.__sides = sides
-
-    def __eq__(self,other):
-        return self.__name == other.__name and self.__sides == other.__sides
-    
-    def __ne__(self,other):
-        return self.__name != other.__name or self.__sides != other.__sides
-    
-    def __str__(self):
-        return f"{self.__name} with sides: {self.__sides}"
-    
-    def calculate_circumference(self):
-        circ = 0
-
-        for side in self.__sides:
-            circ += side
-
-        return circ
-    
+from polygon import Polygon as Polygon_Test
+   
 def test_Polygon_initialization():
     Test_poly = Polygon_Test('triangle',[3,3,3])
     assert Test_poly.get_name() == 'triangle' and Test_poly.get_sides() == [3,3,3]
