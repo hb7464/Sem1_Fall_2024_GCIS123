dc834be3e650664e9c1bcfeb34bf20258975f5d4
hb7464 <hb7464@g.rit.edu>
2024-11-27 13:13:08 +0400

    test

--- activity 4/test_polygon.py
+++ activity 4/test_polygon.py
@@ -5,23 +5,11 @@ def test_Polygon_initialization():
     polygon1 = Polygon("Triangle", [5,5,5])
     assert polygon1.get_name() == "Triangle"
     assert polygon1.get_sides() == [5,5,5]
-
-def test_get_name():
-    polygon1 = Polygon("Pentagon", [5,5,5])
-    assert polygon1.get_name() == "Pentagon"
-
-def test_set_name():
-    polygon1 = Polygon("Triangle", [5,5,5])
-    polygon1.set_name("Pentagon")    
-    assert polygon1.get_name() == "Pentagon"
-
-def test_get_sides():
-    polygon1 = Polygon("Triangle", [5,5,5])
-    assert polygon1.get_sides() == [5,5,5]
-
-def test_set_sides():
-    polygon1 = Polygon("Triangle", [5,5,5])
+    
+    polygon1.set_name("Pentagon")
     polygon1.set_sides([3,3,3,3,3])
+    
+    assert polygon1.get_name() == "Pentagon"
     assert polygon1.get_sides() == [3,3,3,3,3]
 
 def test_polygon_equality():
